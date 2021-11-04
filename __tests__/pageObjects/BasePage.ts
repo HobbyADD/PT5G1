b67e90c5b098fed35eef9e5a007208b1c8a2c9dd
ShaylaZimmerman <shaylarose16@gmail.com>
2021-11-03 21:21:42 -0500

    Base Page with requested revisions

--- __tests__/pageObjects/BasePage.ts
+++ __tests__/pageObjects/BasePage.ts
@@ -1,4 +1,4 @@
-import { By, until, WebDriver } from "selenium-webdriver";
+import { By, until, WebDriver, WebElement } from "selenium-webdriver";
 const chromedriver = require("chromedriver");
 
 export class BasePage {
@@ -10,27 +10,33 @@ export class BasePage {
         this.url = url
     }
 
-    async navigate(): Promise<void> {
-        return await this.driver.get(this.url)
+    async navigate(url?: string): Promise<void> {
+        if (url) return await this.driver.get(url);
+        else if (this.url) return await this.driver.get(this.url);
+        else
+          return Promise.reject(
+            "BasePage.navigate() needs a URL defined on the page object, or one passed in. No URL was provided."
+          );
     }
 
     /**
      * waits for the identified element to be located and visible before returning it.
      * @param {By} elementBy - the locator for the element to return.
      */
-    async getElement(elementBy: By) {
+     async getElement(elementBy: By): Promise<WebElement> {
         await this.driver.wait(until.elementLocated(elementBy));
         let element = await this.driver.findElement(elementBy);
         await this.driver.wait(until.elementIsVisible(element));
         return element;
     }
-
     /**
      * clicks the given element after waiting for it
      * @param {By} elementBy - the locator for the element to click
      */
-    async click(elementBy: By) {
-        return (await this.getElement(elementBy)).click();
+    async click(elementBy: By): Promise<void> {
+        let element = await this.getElement(elementBy);
+        await this.driver.wait(until.elementIsEnabled(element));
+        return await element.click();
     }
 
     /**
@@ -38,8 +44,9 @@ export class BasePage {
      * @param {By} elementBy - the locator for the element to clear and sendKeys to
      * @param {any} keys - the string or list of keys to send
      */
-    async setInput(elementBy: By, keys: any) {
+    async setInput(elementBy: By, keys: any): Promise<void> {
         let input = await this.getElement(elementBy);
+        await this.driver.wait(until.elementIsEnabled(input));
         await input.clear();
         return input.sendKeys(keys);
     }
@@ -48,7 +55,9 @@ export class BasePage {
      * returns an element's text after waiting for it to be visible
      * @param {By} elementBy - the locator of the element to get text from
      */
-    async getText(elementBy: By) {
-        return (await this.getElement(elementBy)).getText();
+    async getText(elementBy: By): Promise<string> {
+        let element = await this.getElement(elementBy);
+        await this.driver.wait(until.elementIsEnabled(element));
+        return element.getText();
     }
 }
