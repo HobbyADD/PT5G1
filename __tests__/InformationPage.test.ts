2a6e083f2f69683014ff7357641acc313bee2811
unknown <awcorri@gmail.com>
2021-11-09 13:55:58 -0500

    InformationPage tested and InfoPage.ts removed

--- __tests__/InformationPage.test.ts
+++ __tests__/InformationPage.test.ts
@@ -4,7 +4,8 @@ import { urlIs } from "selenium-webdriver/lib/until";
 import { getParsedCommandLineOfConfigFile, WatchDirectoryFlags } from "typescript";
 import { HomePage } from "./pageObjects/HomePage";
 
-
+//this is opening the browser to the home page and going to the home page before an async is executed
+//it then loops until there are no more async functions. at that point it closes the browser
 describe("Information Page Navigation", () => {
     const page = new HomePage();
     beforeEach(async () => {
@@ -18,25 +19,36 @@ describe("Information Page Navigation", () => {
 //This tests the Patient and visitor guide page opened
 test("Patient and Visitor Guide page opened", async () => {
     
-    // await page.driver.get(myUrl)
-    await page.clickPatientVisitor()
-    expect(page.isPatientGuidePage).toBeTruthy()
+    //this is clicking on the link to the Patient Visitor page
+    await page.clickPatientVisitor();
+
+    //setting variable isPatientVisitorPageTrue to the HomePage file element locator
+    let isPatientVisitorPageTrue = await page.isPatientGuidePage();
+    expect(isPatientVisitorPageTrue).toBe(true);
     
     })
 
 //This tests the Contact Us page opened
 test("Contact Us page opened", async () => {
     
-    await page.clickContactUs()
-    expect(page.isContactPage).toBeTruthy()
+    //this is clicking on the link to the Contact Us page
+    await page.clickContactUs();
+
+    //setting variable isContactPageTrue to the HomePage file element locator
+    let isContactPageTrue = await page.isContactPage();
+    expect(isContactPageTrue).toBe(true);
     
     })
 
 //This tests the COVID 19 page opened
 test("COVID 19 Info page opened", async () => {
 
-    await page.clickCovidInfo()
-    expect(page.isCovidPage).toBeTruthy()
+    //this is clicking on the link to the COVID 19 page
+    await page.clickCovidInfo();
+
+    //setting variable isCovidPageTrue to the HomePage file element locator
+    let isCovidPageTrue = await page.isCovidPage();
+    expect(isCovidPageTrue).toBe(true);
     
     })
 })
